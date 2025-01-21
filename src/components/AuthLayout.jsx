f596ae4c178fc963cd3e8fc0ce7cb3bfe8e3638d
tusharn3115 <negitushar81@gmail.com>
2025-01-21 11:48:10 +0530

    implement Protected component for authentication handling in AuthLayout; add loading state and navigation logic based on auth status

--- src/components/AuthLayout.jsx
+++ src/components/AuthLayout.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { set } from "react-hook-form";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
-const AuthLayout = () => {
-  return (
-    <div>AuthLayout</div>
-  )
-}
+export default Protected = ({ children, authentication = true }) => {
+  const navigate = useNavigate();
+  const [loader, setLoader] = useState(true);
 
-export default AuthLayout
+  const authStatus = useSelector((state) => state.auth.status);
+
+  useEffect(() => {
+    if (authentication && authStatus !== authentication) {
+      navigate("/login");
+    } else if (!authentication && authStatus !== authentication) {
+      navigate("/");
+    }
+
+    setLoader(false);
+  }, [authStatus, navigate, authentication]);
+
+  return loader ? <h1>Loading...</h1> : <>{children}</>;
+};
