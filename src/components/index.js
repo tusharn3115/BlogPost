c61175cdd8aec9de9d22ac81b401be88eb3d5338
tusharn3115 <negitushar81@gmail.com>
2025-01-22 19:07:47 +0530

    add RTE component for rich text editing; implement PostForm for post creation and editing with image upload functionality

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,7 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container"
 import Logo from "./Logo"
 import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
 
 
 export {
@@ -11,4 +12,5 @@ export {
     Container,
     Logo,
     LogoutBtn,
+    RTE,
 }
